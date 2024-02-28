3f943d71e0b656b5ff6d1bf75014653d27b6431d
ViniciusCosmos <viniciuscosmosilva@gmail.com>
2024-02-28 18:06:13 -0300

    Adicionando classe Department

---
+++ src/application/Main.java
@@ -0,0 +1,15 @@
+package application;
+
+import entities.Department;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Department dpt = new Department(1, "Eletronicos");
+		System.out.println(dpt);
+		
+		
+
+	}
+
+}
